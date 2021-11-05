d91d1a50a785c7053df470c114ed036201a4b7b6
Ryan Hadley <68791163+eaglesTear@users.noreply.github.com>
2021-11-05 18:59:36 +0000

    Add files via upload

---
+++ styles-behavior/main_blog.js
@@ -0,0 +1,100 @@
+//Function to hide and show answers in aside left. How to loop this / compress?
+    function showListAnswer1() {
+    const x = document.getElementById("answer1");
+    if (x.style.visibility === "visible") {
+        x.style.visibility = "hidden";
+    } 
+    else {
+        x.style.visibility = "visible";
+    }
+}
+    
+    function showListAnswer2() {
+    const x = document.getElementById("answer2");
+    if (x.style.visibility === "visible") {
+        x.style.visibility = "hidden";
+    } 
+    else {
+        x.style.visibility = "visible";
+    }
+}
+    
+    function showListAnswer3() {
+    const x = document.getElementById("answer3");
+    if (x.style.visibility === "visible") {
+        x.style.visibility = "hidden";
+    } 
+    else {
+        x.style.visibility = "visible";
+    }
+}
+    
+    function showListAnswer4() {
+    const x = document.getElementById("answer4");
+    if (x.style.visibility === "visible") {
+        x.style.visibility = "hidden";
+    } 
+    else {
+        x.style.visibility = "visible";
+    }
+}
+    
+    function showListAnswer5() {
+    const x = document.getElementById("answer5");
+    if (x.style.visibility === "visible") {
+        x.style.visibility = "hidden";
+    } 
+    else {
+        x.style.visibility = "visible";
+    }
+}
+    
+    function showListAnswer6() {
+    const x = document.getElementById("answer6");
+    if (x.style.visibility === "visible") {
+        x.style.visibility = "hidden";
+    } 
+    else {
+        x.style.visibility = "visible";
+    }
+}
+    
+    function showListAnswer7() {
+    const x = document.getElementById("answer7");
+    if (x.style.visibility === "visible") {
+        x.style.visibility = "hidden";
+    } 
+    else {
+        x.style.visibility = "visible";
+    }
+}
+    
+    function showListAnswer8() {
+    const x = document.getElementById("answer8");
+    if (x.style.visibility === "visible") {
+        x.style.visibility = "hidden";
+    } 
+    else {
+        x.style.visibility = "visible";
+    }
+}
+    
+    function showListAnswer9() {
+    const x = document.getElementById("answer9");
+    if (x.style.visibility === "visible") {
+        x.style.visibility = "hidden";
+    } 
+    else {
+        x.style.visibility = "visible";
+    }
+}
+    
+    function showListAnswer10() {
+    const x = document.getElementById("answer10");
+    if (x.style.visibility === "visible") {
+        x.style.visibility = "hidden";
+    } 
+    else {
+        x.style.visibility = "visible";
+    }
+}
